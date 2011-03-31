1d8e5a95795fac676cbb4b6b2c521a3bb1e79672
Peter Behr <pbehr@uchicago.edu>
2011-03-31 11:19:43 -0700

    Reorganized by type, some changes to templates and defaults as well.

---
+++ js/default.js
@@ -0,0 +1,18 @@
+<script type="text/javascript">
+
+/*
+
+<!--[if lt IE 9]>
+<script>
+  var e = ("abbr,article,aside,audio,canvas,datalist,details," +
+    "figure,footer,header,hgroup,mark,menu,meter,nav,output," +
+    "progress,section,time,video").split(',');
+  for (var i = 0; i < e.length; i++) {
+    document.createElement(e[i]);
+  }
+</script>
+<![endif]-->
+
+*/
+
+</script>
---
+++ js/template.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+
+// single quotes
+// Logging: Venkman, fvlogger
+// the closure ()()
+// js cookies
+// decide on whitespace function()
+// http://expressjs.com/guide.html
+
+</script>
