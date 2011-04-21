ac6d9179d79f57a32e3239c6cc28b8a325f84b67
Peter Behr <pbehr@uchicago.edu>
2011-04-20 23:44:32 -0700

    Added Google Analytics.

--- js/default.js
+++ js/default.js
@@ -2,6 +2,18 @@
 
 /*
 
+// single quotes
+// Logging: Venkman, fvlogger
+// the closure ()()
+// js cookies
+// decide on whitespace function()
+// http://expressjs.com/guide.html
+// semicolons?
+// ajax utilities
+// inside or outside document window
+// browser detection
+// browser html5 element support
+
 <!--[if lt IE 9]>
 <script>
   var e = ("abbr,article,aside,audio,canvas,datalist,details," +
@@ -13,22 +25,37 @@
 </script>
 <![endif]-->
 
-// single quotes
-// Logging: Venkman, fvlogger
-// the closure ()()
-// js cookies
-// decide on whitespace function()
-// http://expressjs.com/guide.html
-// semicolons?
-// ajax utilities
-// inside or outside document window
-
 */
+
+function import_js(url) {
+    var script = document.createElement('script');
+    script.type('text/javascript');
+    script.src(url);
+    document.body.appendChild(script);
+}
+
+import_js('http://peterbehr.net/biblioteka/js/jquery-1.5.2.min.js');
 
 $('input').focus(function () { // input field, bind on focus
 });
 
 $('input').blur(function () {
 });
+
+/* BEGIN Google Analytics */
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-22850350-1']);
+_gaq.push(['_setDomainName', 'none']);
+_gaq.push(['_setAllowLinker', true]);
+_gaq.push(['_trackPageview']);
+
+(function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+/* END Google Analytics */
 
 </script>
