c023fd9d9d471421e9a0fd4383f3206a80cac7d5
Peter Behr <pbehr@uchicago.edu>
2011-04-22 16:14:34 -0700

    I am a JS nooooob.

--- js/default.js
+++ js/default.js
@@ -1,47 +1,14 @@
 <script type="text/javascript">
 
-/*
-
-// single quotes
-// Logging: Venkman, fvlogger
-// the closure ()()
-// js cookies
-// decide on whitespace function()
-// http://expressjs.com/guide.html
-// semicolons?
-// ajax utilities
-// inside or outside document window
-// browser detection
-// browser html5 element support
-
-<!--[if lt IE 9]>
-<script>
-  var e = ("abbr,article,aside,audio,canvas,datalist,details," +
-    "figure,footer,header,hgroup,mark,menu,meter,nav,output," +
-    "progress,section,time,video").split(',');
-  for (var i = 0; i < e.length; i++) {
-    document.createElement(e[i]);
-  }
-</script>
-<![endif]-->
-
-*/
-
-function import_js(url) {
+function import_js(src) {
     var script = document.createElement('script');
-    script.type('text/javascript');
-    script.src(url);
+    script.type = 'text/javascript';
+    script.src = src;
     document.body.appendChild(script);
 }
 
 import_js('http://peterbehr.net/biblioteka/js/jquery-1.5.2.min.js');
 import_js('http://peterbehr.net/biblioteka/js/jquery_plugin_md5.js');
-
-$('input').focus(function () { // input field, bind on focus
-});
-
-$('input').blur(function () {
-});
 
 /* BEGIN Google Analytics */
 
