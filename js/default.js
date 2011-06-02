f0a6ed49c0e9410222df8c8e2d9005b6389248c9
Peter Behr <pbehr@uchicago.edu>
2011-06-01 17:50:45 -0700

    Condensing.

--- js/default.js
+++ js/default.js
@@ -1,26 +1,11 @@
-/* BEGIN script import */
+/* BEGIN utilities */
 
 function import_js(src, callback) {
-    var length = srcs.length;
-    for (var i = 0; i < length; i++) {
-        var script = document.createElement('script');
-        script.type = 'text/javascript';
-        script.src = srcs[i];
-        var first = document.getElementsByTagName('script')[0]; first.parentNode.insertBefore(script, first);
-    }
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = src;
+    var first = document.getElementsByTagName('script')[0]; first.parentNode.insertBefore(script, first);
     callback();
 }
-
-    import_js(  [
-'http://peterbehr.net/biblioteka/js/others/jquery-1.5.2.min.js',
-'http://peterbehr.net/biblioteka/js/others/jquery_plugin_md5.js'
-                ],
-                function () { return; } );
-
-/* END script import */
-
-
-
-/* BEGIN utilities */
 
 /* END utilities */
