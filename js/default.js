bc68ac87c48e6fbef7207f93bbcbe8a1e052cd30
Peter Behr <code@peterbehr.net>
2012-12-04 18:14:45 +0800

    Working on the JS a bit.

--- js/default.js
+++ js/default.js
@@ -17,10 +17,55 @@ var Biblioteka = {
         script.src = src;
         var first = document.getElementsByTagName('script')[0]; first.parentNode.insertBefore(script, first);
         callback();
-    }
+    },
+    
+    query_hash: function () {
+        var parameters = {};
+        var pairs = location.search.replace('?', '').split('&');
+        if (pairs[0]) {
+            for (var i = 0; i < pairs.length; i++) {
+                pair = pairs[i].split('=');
+                parameters[pair[0]] = pair[1];
+            }
+        }
+        return(parameters);
+    },
     
     // END utilities
     
-}
+    // BEGIN classes
+    
+    Timer: function (interval) {
+        // want a timer object which can be started, stopped, and reset
+        this.timestamp = 0;
+        this.interval = interval || 100;
+        var self;
+        this.start = function () {
+            self = this;
+            self.clock = setInterval(function () {
+                self.timestamp += self.interval;
+            }, self.interval);
+            return(self.timestamp);
+        };
+        this.stop = function () {
+            clearInterval(self.clock);
+            return(self.timestamp);
+        };
+        this.read = function () {
+            return(self.timestamp);
+        };
+        this.clear = function () {
+            self.timestamp = 0;
+            return(self.timestamp);
+        };
+        // can this be done without 'new'?
+        // just a function that returns an instance of itself?
+    },
+    
+    // END classes
+    
+    pass: function () {}
+    
+};
 
-window.Biblioteka = window.Bi = Biblioteka;
+window.Biblioteka = window.B = Biblioteka;
